f90f4fe67adb24690c2257b30b62105bc83fb71d
darshangane <darshangane23@gmail.com>
2025-04-08 00:57:11 +0530

    backend

---
+++ app.js
@@ -0,0 +1,364 @@
+const express = require('express');
+const app = express ();
+const mongoose = require('mongoose');
+const {User} = require('./model/User');
+const morgan = require('morgan');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {Product} = require('./model/Product');
+const {Cart} = require('./model/Cart');
+
+//middleware
+app.use(cors());
+app.use(morgan('dev'))
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/darshanecom')
+.then(()=>{
+    console.log("DB is connected")
+}).catch((error)=>{
+    console.log("DB is not connected", error)
+})
+
+//task-1 create route for register user
+app.post('/register',async(req,res)=>{
+    try{
+        let {name,email,password} = req.body;
+
+        //check if any field is missing
+        if(!email || !name || !password){
+            res.status(400).json({
+                message:"Field is missing"
+            })
+        }
+
+        //check if is user already have account
+
+        const user = await User.findOne({email});
+
+        if(user){
+            return req.status(400).json({
+                message : "User already has account"
+            })
+        }else{
+
+            //hash the password -->  secure password
+            const salt = bcrypt.genSaltSync(10);
+            const hashedPassword = bcrypt.hashSync(password,salt);
+
+            //user authentication
+            const token = jwt.sign({email},"supersecret",{expiresIn:'365d'});
+
+            //create user in database
+            await User.create({
+                name,
+                password : hashedPassword,
+                email,
+                token,
+                role:'user'
+            })
+            
+            return res.status(200).json({
+                message: "User created Successfully"
+            })
+
+        }
+
+    }catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal server error"
+        })
+    }
+})
+
+//task-2 create route for register for login
+
+app.post('/login',async(req,res)=>{
+    try{
+        let {email,password} = req.body;
+
+        //check all fields are there or not
+    if(!email || !password){
+        return res.status(400).json({
+            message: "Field is missing"
+        })
+    }
+
+    //checking user having account
+    const user = await User.findOne({email});
+
+    if(!user){
+        return res.status(400).json({
+            message: "User not registered"
+        })
+    }
+    //compare password with the stored password
+    const isPasswordMatched = bcrypt.compare(password, user.password)
+
+    if(!isPasswordMatched){
+        return res.status(404).json({
+            message: "Password is Wrong"
+        })
+    }
+    
+    return res.status(200).json({
+        message: "User logged in Successfully",
+        id: user._id,
+        name: user.name,
+        token: user.token,
+        email: user.email,
+        role: user.role
+    })
+
+    }catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal server error"
+        })
+    }
+})
+
+//task-3 create route to see all product
+app.get('/products',async(req,res)=>{
+    try{
+        // let {name, price, stock, brand, image, description}
+        const products = await Product.find();
+        res.status(200).json({
+            message: "product found Successfully",
+            products:products
+        })
+    }
+    catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal server error"
+        })
+    }
+})
+
+//task-4 create route to add product
+app.post('/add-product',async(req,res)=>{
+    try{
+        let {name, image, description, stock, brand, price}=req.body;
+        const {token} = req.headers;
+        const decodedToken = jwt.verify(token, "supersecret");
+        const user = await User.findOne({email: decodedToken.email})
+        const product = await Product.create({
+            name,
+            stock,
+            price,
+            image,
+            description,
+            brand,
+            user: user._id
+        })
+        return res.status(200).json({
+            message: "Product created Successfully",
+            product:product
+        })
+    }
+    catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal server error"
+        })
+    }
+    })
+
+//task-5 create route to see particular product
+app.get('/product/:id',async(req,res)=>{
+    try{
+        let {id} = req.params;
+        if(!id){
+            return res.status(400).json({
+                message: "Product id not Found"
+            })
+        }
+        let {token} = req.headers;
+        const decodedToken = jwt.verify(token, "supersecret");
+        
+        if(decodedToken.email){
+            const product = await Product.findById(id);
+
+            if(!product){
+                res.status(400).json({
+                    message: "Product not Found"
+                })
+            }
+            return res.status(200).json({
+                message: "Product found Successfully",
+                product
+            })
+        }
+
+    }catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal server error"
+        })
+    }
+})
+
+//task-6 create route to update product
+app.patch('/product/edit/:id',async(req,res)=>{
+  
+        const {id} = req.params;
+        const {name, image, stock, brand, description, price} = req.body.productData;
+        const {token} = req.headers;
+        const decodedToken = jwt.verify(token, "supersecret");
+        try{
+        if(decodedToken.email){
+            const updatedProduct = await Product.findByIdAndUpdate(id, {
+                name,
+                brand,
+                description,
+                image,
+                price,  
+                stock ,
+            });
+            res.status(200).json({
+                message: "Product Updated Successfully",
+                product: updatedProduct
+            });
+        }
+    }catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal server error",
+        });
+    }
+
+});
+
+//task-7 --> create route to delete product
+app.delete('/product/delete/:id',async(req,res)=>{
+    try{
+        let {id} = req.params;
+        if(!id){
+            return res.status(400).json({
+                message: "Id not Found"
+            })
+        }
+        let deletedProduct = await Product.findByIdAndDelete(id);
+        if(!deletedProduct){
+            return res.status(400).json({
+                message : "Product not Found"
+            })
+        }
+        return res.status(200).json({
+            message: "Product deleted Successfully",
+            product:deletedProduct
+        })
+    }
+        catch(error){
+            console.log(error);
+            res.status(400).json({
+                message:"Internal server error",
+            });
+        }
+})
+
+//task-8 --> create route to see all product in cart
+app.get('/cart',async(req,res)=>{
+    try{
+        let {token} = req.headers;
+        const decodedToken = jwt.verify(token, "supersecret")
+        const user = await User.findOne({email:decodedToken.email}).populate({
+            path:'cart',
+            populate:{
+                path: 'products',
+                model: 'Product'
+            }
+        })
+        if(!user){
+            return res.status(400).json({
+                message: "User not Found"
+            })
+        }
+        return res.status(200).json({
+            cart: user.cart
+        })
+
+    }
+    catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal Server Error",
+        });
+    }
+})
+
+//task-9 --> create route to see all product in cart
+app.post('/cart/add', async(req,res)=>{
+    try{
+       const body = req.body;
+       //getting product if from frontend
+       const productArray = body.products;
+       let totalPrice = 0;
+       
+       //find the product and add product price in total
+       for(let item of productArray){
+        const product = await Product.findById(item);
+        if(product){
+            totalPrice += product.price;
+
+        }
+       }
+       const {token} = req.headers;
+       const decodedToken = jwt.verify(token, "supersecret")
+       const user = await User.findOne({email:decodedToken.email});
+       if(!user){
+        return res.status(400).json({
+            message: "User not Found"
+        })
+       }
+
+       //checking if user already has a cart
+       let cart;
+       if(user.cart){
+        cart = await Cart.findById(user.cart).populate('products');
+        const existingProductsIds = cart.products.map((product)=>{
+            product._id.toString()
+        })
+
+        //if product is not already in the cart, add it to cart
+        productArray.forEach(async(productId)=>{
+            if(!existingProductsIds.includes(productId)){
+                cart.products.push(productId);
+                const product = await Product.findById(productId);
+                totalPrice += product.price;
+            }
+        })
+        //UPDATING CART.TOTAL with the new total price
+        cart.total = totalPrice;
+        await cart.save();   
+    }else{
+        //create new cart
+        cart = new Cart({
+            products:productArray,
+            total: totalPrice
+
+        })
+        await cart.save();
+        user.cart = cart._id;
+        await user.save();
+     }
+     return res.status(200).json({
+        message: "Product Added to cart Successfully",
+        cart:cart
+    })
+    }
+    catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal server error"
+        });
+    }
+})
+
+let PORT = 8080;
+app.listen(PORT,()=>{
+    console.log(`server is connected to port ${PORT}`)
+}) 
